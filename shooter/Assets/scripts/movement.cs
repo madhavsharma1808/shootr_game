318bb23bec170014fd3eaa0283769dc6178edb31
madhavsharma1808 <madhav.189302106@muj.manipal.edu>
2020-01-21 21:23:42 +0530

    added basic components

---
+++ shooter/Assets/scripts/movement.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class movement : MonoBehaviour
+{
+    [SerializeField] float increase=10f;
+    [SerializeField] float padding=1f;
+    float xmin, xmax, ymin, ymax;
+    // Start is called before the first frame update
+    void Start()
+    {
+        cameraset();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        move();
+    }
+
+    private void cameraset()
+    {
+        Camera gamecamera = Camera.main;
+         xmin = gamecamera.ViewportToWorldPoint(new Vector3(0, 0, 0)).x+padding;
+         xmax = gamecamera.ViewportToWorldPoint(new Vector3(1,0,0)).x-padding;
+         ymin = gamecamera.ViewportToWorldPoint(new Vector3(0, 0, 0)).y+padding;
+         ymax = gamecamera.ViewportToWorldPoint(new Vector3(0, 1, 0)).y-padding;
+
+    }
+
+    private void move()
+    {
+        var delx = Input.GetAxis("Horizontal")*Time.deltaTime*increase;
+        var nextposx = Mathf.Clamp(transform.position.x + delx,xmin,xmax);
+        var dely = Input.GetAxis("Vertical")*Time.deltaTime*increase;
+        var nextposy = Mathf.Clamp(transform.position.y + dely,ymin,ymax);
+        transform.position = new Vector2(nextposx,nextposy);
+
+    }
+}
