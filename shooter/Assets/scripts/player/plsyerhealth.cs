11c0c345f5f1ddbc3b3ca0a744d567be64375f9b
madhavsharma1808 <madhav.189302106@muj.manipal.edu>
2020-01-31 22:12:36 +0530

    added home page and game over page

---
+++ shooter/Assets/scripts/player/plsyerhealth.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class plsyerhealth : MonoBehaviour
+{
+   
+    // Start is called before the first frame update
+    [SerializeField] int playerhealth=10;
+    void Start()
+    {
+        
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        Debug.Log("WELCOME");
+       if(playerhealth>0)
+        {
+            playerhealth--;
+        }
+        else if(playerhealth<=0)
+        {
+            Destroy(gameObject);
+            SceneManager.LoadScene("GAME OVER");
+        }
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
